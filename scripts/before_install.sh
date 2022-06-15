f2ae20dc5ba12fba14507b5f4ee65bcb0390680f
fchinch <fchinchillahn@gmail.com>
2022-06-15 13:48:27 -0600

    Fix bashes 12

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+      . ~/.nvm/nvm.sh
+nvm uninstall v18.0.0
+nvm install --lts
+nvm use --lts
+node -e "console.log('Running Node.js ' + process.version)"
+npm install yarn
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
