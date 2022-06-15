35eb6517cb3cc06037b1733c8212d1eaaab00c3e
fchinch <fchinchillahn@gmail.com>
2022-06-15 10:57:30 -0600

    Fix bashes 2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
-#give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/express-app
 
-#navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 #install node modules
 npm install
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+node app.js > app.out.log 2> app.err.log < /dev/null &
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#download node and npm
+#download
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install node 
 
-#create our working directory if it doesnt exist
+#create our working directory if it doesn't exist
 DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
+if [  -d "$DIR" ]; then
+    echo "Directory ${DIR} exists"
+ else 
+    echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
+
