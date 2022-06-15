43d5b42dd8ab57e49f2bcf72e6aa40403a92ff2f
fchinch <fchinchillahn@gmail.com>
2022-06-15 10:52:01 -0600

    Fix bashes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#download
+#download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node 
+nvm install node
 
-#create our working directory if it doesn't exist
+#create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
-if [  -d "$DIR" ]; then
-    echo "Directory $DIR exists"
- else 
-  echo "Creating directory $DIR"
-  mkdir $DIR
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
+else
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
-
