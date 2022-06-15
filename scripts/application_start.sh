49d978c505ea0a00b0db2b016683f287b14544ff
fchinch <fchinchillahn@gmail.com>
2022-06-15 14:09:18 -0600

    Fix bashes 15

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,8 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/express-app
 
+source /home/ec2-user/.bash_profile
+
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
 
